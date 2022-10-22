72c2b88dbf0a4f30dcdd58b90e334caa31d56628
FJohnHRoss <rossjohn936@gmail.com>
2022-10-21 20:42:17 -0700

    forLoopBasic1

---
+++ week1/day1/forLoopBasic1/for_loop_basic1.py
@@ -0,0 +1,41 @@
+# 1
+
+for i in range(1, 151):
+    print(i)
+
+# 2
+
+for i in range(5, 1001, 5):
+    print(i)
+
+# 3
+
+for i in range(0, 101, 5):
+    if i % 10 == 0:
+        print("Coding Dojo")
+    else:
+        print("Coding")
+
+
+# 4
+
+add = 0
+for i in range(500000):
+    add += i
+
+print(add)
+
+# 5
+
+for i in range(2018, 0, -4):
+    print(i)
+
+# 6
+
+lowNum = 2
+highNum = 9
+mult = 3
+
+for i in range(2, 10):
+    if i % 3 == 0:
+        print(i)
