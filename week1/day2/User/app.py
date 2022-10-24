75f5974ecee8577249e2a4bbc733209ea63aa6ae
FJohnHRoss <rossjohn936@gmail.com>
2022-10-24 12:51:40 -0700

    update

--- week1/day2/User/app.py
+++ week1/day2/User/app.py
@@ -13,23 +13,22 @@ class User:
     def enroll(self):
         if self.gold_card_points > 200:
             self.is_rewards_member = True
+            return self
         else:
             self.is_rewards_member = False
+            return self
 
     def spend_points(self, amount):
         if amount - self.gold_card_points < 0:
             self.gold_card_points -= amount
+            return self
+        else:
+            return self
 
 
 user1 = User("John", "Ross", "email@gmail.com", 20)
 user2 = User("Kaija", "Pendergast", "dummy@gmail.com", 50)
 user3 = User("Chandler", "McAdory", "commaAprentis@gmail.com", 25)
-user1.spend_points(50)
-user1.enroll()
-user1.display_info()
-user2.spend_points(80)
-user2.enroll()
-user2.display_info()
-user3.spend_points(0)
-user3.enroll()
-user3.display_info()
+user1.spend_points(50).enroll().display_info()
+user2.spend_points(80).enroll().display_info()
+user3.spend_points(0).enroll().display_info()
