65e484a424e0c04c1d63baabc9574acf7b2a4568
FJohnHRoss <rossjohn936@gmail.com>
2022-10-24 18:36:14 -0700

    users_bank_acc

--- week1/day2/Users_Bank_Account/app.py
+++ week1/day2/Users_Bank_Account/app.py
@@ -1,3 +1,6 @@
+from cmath import log
+
+
 class BankAccount:
 
     def __init__(self, int_rate, balance):
@@ -29,18 +32,31 @@ class BankAccount:
 # bank1 = BankAccount(0.2, 10)
 # bank1.deposit(1).deposit(2).deposit(2).withdraw(
 #     20).yield_interest().display_account_info()
-# bank2 = BankAccount(0.1, 1004)
+# bank2 = BankAccount(0.02, 1004)
 # bank2.deposit(1632).deposit(1875).withdraw(782).withdraw(976).withdraw(
 #     703).withdraw(202).yield_interest().display_account_info()
 
 
 class User:
-    def __init__(self, name, email):
+    def __init__(self, name, email, balance):
         self.name = name
         self.email = email
-        self.account = BankAccount(int_rate=0.02, balance=0)
-
-    # other methods
+        self.account = BankAccount(0.02, balance)
 
     def make_deposit(self, amount):
-        # your code here
+        self.account.deposit(amount)
+        print(f"{self.name}Deposited: {amount}")
+        return self
+
+    def make_withdrawal(self, amount):
+        self.account.withdraw(amount)
+        print(f"{self.name}Withdrawed: {amount}")
+        return self
+
+    def display_user_balance(self):
+        self.account.display_account_info()
+        return self
+
+
+user1 = User("John ", "email@test.com", 2095)
+user1.make_deposit(19).make_withdrawal(1005).display_user_balance()
