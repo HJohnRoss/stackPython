3e981e7084cbaddc60def3a001ac1df4e2b96350
FJohnHRoss <rossjohn936@gmail.com>
2022-10-25 20:18:04 -0700

    notes

--- week1/day4/hello_flask/server.py
+++ week1/day4/hello_flask/server.py
@@ -1,21 +1,21 @@
-from flask import Flask  # Import Flask to allow us to create our app
-# Create a new instance of the Flask class called "app"
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-# The "@" decorator associates this route with the function immediately following
 @app.route('/')
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return render_template("index.html")
 
 
 @app.route("/success")
 def success():
     return "Success"
 
+
 @app.route('/hello/<string:user>/<int:num>')
 def hello(user, num):
-    return f"hello {user * num} "
+    return render_template("hello.html", user=user, num=num)
 
-if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
-    app.run(debug=True)    # Run the app in debug mode.
+
+if __name__ == "__main__":
+    app.run(debug=True)
