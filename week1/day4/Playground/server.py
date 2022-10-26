3b4ec87b87b633babf0ed301d0020f3f3e5d4444
FJohnHRoss <rossjohn936@gmail.com>
2022-10-26 07:42:39 -0700

    playground

--- week1/day4/Playground/server.py
+++ week1/day4/Playground/server.py
@@ -2,16 +2,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/play')
-def main():
-    return render_template("index.html") #cant set num = to something or 
+# def main():
+#     return render_template("index.html", num=3) #cant set num = to something or 
   # or an if statement to have 3 show for /play and for loop for else
   
-@app.route('/play/<int:num>')
-def addBox(num):
-    return render_template("index.html", num=num)
+# def addBox(num):
+#     return render_template("index.html", num=num)
 
 # how to plug in classes into divs
+
+@app.route('/play')
+@app.route('/play/')
+@app.route('/play/<int:num>')
+@app.route('/play/<int:num>/<string:color>')
+def add_bg_color(num=3, color="#9EC5F7"):
+    return render_template("index.html", num=num, color=color)
 
 
 if __name__ == "__main__":
