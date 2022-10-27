ef060507209c4a67e3b0e2c807827e9fcbdc9c4c
FJohnHRoss <rossjohn936@gmail.com>
2022-10-27 11:10:42 -0700

    HTML Table

--- week1/day4/hello_flask/server.py
+++ week1/day4/hello_flask/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return "hello"
 
 
 @app.route("/success")
@@ -12,7 +12,7 @@ def success():
     return "Success"
 
 
-@app.route('/hello/<string:user>/<int:num>')
+@app.route('/hello/<user>/<int:num>')
 def hello(user, num):
     return render_template("hello.html", user=user, num=num)
 
